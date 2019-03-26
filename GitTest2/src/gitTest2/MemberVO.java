cd63eb2129b850428d4e93200ca2f85ebeb577e7
jyhynn <letsjamm@naver.com>
2019-03-26 16:32:44 +0900

    MemberVO 생성

---
+++ GitTest2/src/gitTest2/MemberVO.java
@@ -0,0 +1,22 @@
+package gitTest2;
+
+public class MemberVO {
+	private String name, id;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+}
